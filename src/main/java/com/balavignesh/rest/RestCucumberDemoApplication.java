14d8c4806aa477dcd1f78c6b2427471f9b3b0c6a
Bala <balavignesh_b@live.com>
2021-03-05 18:06:06 -0500

    Initial commit

---
+++ src/main/java/com/balavignesh/rest/RestCucumberDemoApplication.java
@@ -0,0 +1,13 @@
+package com.balavignesh.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCucumberDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCucumberDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balavignesh/rest/RestCucumberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balavignesh.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCucumberDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
